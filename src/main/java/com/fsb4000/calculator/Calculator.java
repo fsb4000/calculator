6fbf62a427020a8efa72d265e1a2018e7fc66027
fsb4000 <fsb4000@yandex.ru>
2018-11-01 15:15:04 -0400

    Add static analisis stage

--- src/main/java/com/fsb4000/calculator/Calculator.java
+++ src/main/java/com/fsb4000/calculator/Calculator.java
@@ -2,6 +2,9 @@ package com.fsb4000.calculator;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator logic class
+ */
 @Service
 public class Calculator {
     int sum(int a, int b) {
