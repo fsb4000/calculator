fcdba00a88d1eb275da7d992fbc1cb911753e49f
fsb4000 <fsb4000@yandex.ru>
2018-11-01 00:44:35 -0400

    Add sum logic, controller and unit test

---
+++ src/main/java/com/fsb4000/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.fsb4000.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
